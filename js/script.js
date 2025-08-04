f16edf0f3308a98fa951b09b2f10cda8ebf96ed0
iliya amani wxe <iliya.amani06@gmail.com>
2025-08-04 15:11:54 +0330

    dark mood & box

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@
     const activeLink = document.querySelector('.custom-tabs .nav-link.active');
     moveUnderline(activeLink);
   });
+  document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll('.inner-tab');
+    const pages = document.querySelectorAll('.inner-page');
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+        const targetId = tab.getAttribute('data-target');
+        pages.forEach(p => {
+          if (p.id === targetId) {
+            p.classList.add('active');
+          } else {
+            p.classList.remove('active');
+          }
+        });
+      });
+    });
+  });
